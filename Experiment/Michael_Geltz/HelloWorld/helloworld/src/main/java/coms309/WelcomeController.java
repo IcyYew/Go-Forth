fd40dc0574c0c6d6547b423b291f6066d78295ae
Michael <mgeltz@iastate.edu>
2024-02-02 08:50:38 -0600

    Changed to display local date after name introduction

--- Experiment/Michael_Geltz/HelloWorld/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiment/Michael_Geltz/HelloWorld/helloworld/src/main/java/coms309/WelcomeController.java
@@ -1,8 +1,12 @@
 package coms309;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 class WelcomeController {
@@ -14,6 +18,6 @@ class WelcomeController {
 
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
-        return "Hello and welcome to COMS 309: " + name;
+        return "Hello and welcome to COMS 309: " + name + " on " + LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
     }
 }
