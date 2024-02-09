61c994e922025effcc605ab9143cfc5bdced2255
Michael <mgeltz@iastate.edu>
2024-02-09 12:35:01 -0600

    Fixed welcome class weird git stash thing

--- Backend/app/src/main/java/com/spring/app/Welcome.java
+++ Backend/app/src/main/java/com/spring/app/Welcome.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.spring.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,19 +12,4 @@ public class Welcome {
         return "Welcome to troop battle sim temporary";
     }
 }
-=======
-package com.spring.app;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-
-
-public class Welcome {
-    @GetMapping("/")
-    public String welcome() {
-        return "Welcome to troop battle sim test";
-    }
-}
->>>>>>> 95d6935c0edc44060bfcf3086af22c741e8c44f1
