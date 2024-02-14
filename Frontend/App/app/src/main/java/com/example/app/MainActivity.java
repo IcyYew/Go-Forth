8eab9b81c4563bab867113ee0f63ee8d39c3e471
Nicholas Lynch <ndlynch@iastate.edu>
2024-02-14 12:06:23 -0600

    Added functionality to MainActivity in frontend

--- Frontend/App/app/src/main/java/com/example/app/MainActivity.java
+++ Frontend/App/app/src/main/java/com/example/app/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;     // define login button variable
+    private Button signupButton;    // define signup button variable
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButton = findViewById(R.id.Login);
+        signupButton = findViewById(R.id.Signup);
+
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, "Login Pressed", Toast.LENGTH_SHORT);
+                toast.show();
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+                //Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, "Signup Pressed", Toast.LENGTH_SHORT);
+                toast.show();
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+                //Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                //startActivity(intent);
+            }
+        });
     }
 }
