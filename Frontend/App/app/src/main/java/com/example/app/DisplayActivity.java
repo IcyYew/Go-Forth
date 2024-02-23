b2fbc0522d6753189f55444dfcf23152a1bc99cf
jddwight@iastate.edu <jddwight@iastate.edu>
2024-02-23 15:02:13 -0600

    Refining display

--- Frontend/App/app/src/main/java/com/example/app/DisplayActivity.java
+++ Frontend/App/app/src/main/java/com/example/app/DisplayActivity.java
@@ -1,7 +1,35 @@
 package com.example.app;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayActivity extends AppCompatActivity {
+    private EditText ID;
+    private TextView users;
+    private Button confirm;
+    private Button back;
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_users);
+
+        ID = findViewById(R.id.IDInput);
+        users = findViewById(R.id.msgResponse);
+        back = findViewById(R.id.back);
+        confirm = findViewById(R.id.confirm);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DisplayActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
