f3f30d1c0dc8d6c28114ace0832bbbe1090fa041
Nicholas Lynch <ndlynch@iastate.edu>
2024-02-28 14:29:27 -0600

    Login and Signup functional :)

--- Frontend/App/app/src/main/java/com/example/app/LoginActivity.java
+++ Frontend/App/app/src/main/java/com/example/app/LoginActivity.java
@@ -4,10 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText Username;
@@ -18,9 +30,17 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button Back;
 
+    private int userID;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            userID = extras.getInt("ID");
+        }
+
 
         //Initialize UI
         Username = findViewById(R.id.username);
@@ -36,6 +56,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("ID", String.valueOf(userID));
                 startActivity(intent);
             }
         });
@@ -43,28 +64,53 @@ public class LoginActivity extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String url = "http://10.0.2.2:8080/players/getall";
                 String usernameString = Username.getText().toString();
                 String passwordString = Password.getText().toString();
-                //TODO: Get actual username and password data from backend
-                if(usernameString.equals("username")){
-                    if(passwordString.equals("password")){
-                        //username and password match
-                        Toast toast = Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                    else{
-                        //username exist but password does not match
-                        Toast toast = Toast.makeText(LoginActivity.this, "Username or password do not exist (Password)", Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                }
-                else{
-                    //username does not exist
-                    Toast toast = Toast.makeText(LoginActivity.this, "Username or password do not exist (Username)", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
+                // make a StringRequest to get the users from the server. Converts JSONArray into StringBuilder.
+                StringRequest request = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Log.d("Display response", response);
+                                try {
+                                    JSONArray jsonArray = new JSONArray(response);
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        JSONObject playerObject = jsonArray.getJSONObject(i);
+                                        if (playerObject.getString("userName").equals(usernameString))
+                                            if (playerObject.getString("password").equals(passwordString)) {
+                                                //username and password match
+                                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                intent.putExtra("ID", playerObject.getString("playerID"));
+                                                startActivity(intent);  // go to LoginActivity
+                                                return;
+                                            } else {
+                                                //username exist but password does not match
+                                                Toast toast = Toast.makeText(LoginActivity.this, "Username or password do not exist (Password)", Toast.LENGTH_SHORT);
+                                                toast.show();
+                                                return;
+                                            }
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                //username does not exist
+                                Toast toast = Toast.makeText(LoginActivity.this, "Username or password do not exist (Username)", Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), "Error fetching players: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                // add to the request queue
+                VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+
             }
         });
-
     }
 }
