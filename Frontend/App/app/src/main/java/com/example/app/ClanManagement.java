479645c2f25f9f17231148f6e80fe081d2993a5e
Nicholas Lynch <ndlynch@iastate.edu>
2024-03-22 14:36:24 -0500

    Added Clan Management activites, layouts for Clan and Clan Chat actvity, and started layouts for Clan Management and Clan Join

---
+++ Frontend/App/app/src/main/java/com/example/app/ClanManagement.java
@@ -0,0 +1,24 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Objects;
+
+public class ClanManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clan_management);
+    }
+}
